669f456e8496110a14eda4093c5da12e55b6bb5d
David Vávra <david@vavra.me>
2022-07-10 17:43:43 +0200

    Filter works

--- app/src/main/java/me/vavra/sportresults/viewmodel/SportResultsViewModel.kt
+++ app/src/main/java/me/vavra/sportresults/viewmodel/SportResultsViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
+import me.vavra.sportresults.model.SportResult
 import me.vavra.sportresults.model.SportResultsState
 import me.vavra.sportresults.network.RemoteRepository
 import me.vavra.sportresults.storage.LocalRepository
@@ -21,22 +22,35 @@ class SportResultsViewModel : ViewModel() {
     )
         private set
 
+    private var allSportResults = listOf<SportResult>()
+
     init {
         viewModelScope.launch {
             LocalRepository.observe().combine(RemoteRepository.observe()) { local, remote ->
                 local + remote
             }.collect { merged ->
                 val sorted = merged.sortedByDescending { it.timestamp }
-                state = state.copy(sportResults = sorted)
+                allSportResults = sorted
+                applyFilter()
             }
         }
     }
 
     fun changeShowRemote() {
         state = state.copy(showRemote = !state.showRemote)
+        applyFilter()
     }
 
     fun changeShowLocal() {
         state = state.copy(showLocal = !state.showLocal)
+        applyFilter()
+    }
+
+    private fun applyFilter() {
+        val showNone = !state.showLocal && !state.showRemote
+        val showRemote = state.showRemote || showNone // Showing none is equivalent to showing all
+        val showLocal = state.showLocal || showNone // Showing none is equivalent to showing all
+        val filtered = allSportResults.filter { (it.remote && showRemote) || (!it.remote && showLocal) }
+        state = state.copy(sportResults = filtered)
     }
 }
